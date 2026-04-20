4aa035e06b7b376739eb0eb52550f89c9e8c66fc
jjjjjjisong <wltjdwp12@gmail.com>
2026-04-20 17:27:01 +0900

    Update gubun.ts

--- src/constants/gubun.ts
+++ src/constants/gubun.ts
@@ -1,3 +1,3 @@
-export const GUBUN_OPTIONS = ['컵', '컵뚜껑', '비닐', '스트로우', '기타'] as const;
+export const GUBUN_OPTIONS = ['컵', '컵뚜껑', '비닐', '스트로우', '종이컵', '기타'] as const;
 
 export const DEFAULT_GUBUN = GUBUN_OPTIONS[0];
